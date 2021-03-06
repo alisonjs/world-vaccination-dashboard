2d097be3a150d71e9d4c475b1d1b7325914b7c4d
Alison Silva <alison.dev.silva@gmail.com>
2021-03-05 23:06:39 -0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'', component:DashboardComponent},
+  {path:'**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,43 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { HeaderModule } from './shared/components/header/header.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DashboardModule } from './components/dashboard/dashboard.module';
+import { VaccinationDailyChartComponent } from './components/vaccination-daily-chart/vaccination-daily-chart.component';
+import { VaccinationDailyChartModule } from './components/vaccination-daily-chart/vaccination-daily-chart.module';
+import { VaccinationWorldChartComponent } from './components/vaccination-world-chart/vaccination-world-chart.component';
+import { VaccinationWorldChartModule } from './components/vaccination-world-chart/vaccination-world-chart.module';
+import { VaccinationWorldmapChartComponent } from './components/vaccination-worldmap-chart/vaccination-worldmap-chart.component';
+import { VaccinationWorldmapChartModule } from './components/vaccination-worldmap-chart/vaccination-worldmap-chart.module';
+import { LoginComponent } from './components/login/login.component';
+import { LoginModule } from './components/login/login.module';
+import { DatasetManagerComponent } from './components/dataset-manager/dataset-manager.component';
+import { DatasetManagerModule } from './components/dataset-manager/dataset-manager.module';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PageNotFoundModule } from './components/page-not-found/page-not-found.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    VaccinationDailyChartComponent,
+    VaccinationWorldChartComponent,
+    VaccinationWorldmapChartComponent,
+    LoginComponent,
+    DatasetManagerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     HeaderModule,
-    DashboardModule
+    DashboardModule,
+    VaccinationDailyChartModule,
+    VaccinationWorldChartModule,
+    VaccinationWorldmapChartModule,
+    LoginModule,
+    DatasetManagerModule,
+    PageNotFoundModule
   ],
   providers: [],
   bootstrap: [AppComponent]
