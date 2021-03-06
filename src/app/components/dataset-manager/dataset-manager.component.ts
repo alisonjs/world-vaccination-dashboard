ae09d8573bc20f408e6924cf9203ed6719656bdb
Alison Silva <alison.dev.silva@gmail.com>
2021-03-05 23:06:21 -0300

    Add dataset manager component

---
+++ src/app/components/dataset-manager/dataset-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dataset-manager',
+  templateUrl: './dataset-manager.component.html',
+  styleUrls: ['./dataset-manager.component.css']
+})
+export class DatasetManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
