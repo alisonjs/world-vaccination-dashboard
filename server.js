20077d75c38a0f9cfca7e653ca3c9cacaadb6011
Alison Silva <alison.dev.silva@gmail.com>
2021-03-11 00:05:43 -0300

    Fix deploy config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/world-vaccination-dashboard'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/world-vaccination-dashboard/'}),
